f42f955ef7d3eacf2594c7841684f9f4d929430e
Karter13 <KarMiH1975@mail.ru>
2020-11-27 19:13:57 +0300

    installed redux-saga

--- src/api/todolists-api.ts
+++ src/api/todolists-api.ts
@@ -3,7 +3,7 @@ import axios from 'axios'
 const settings = {
     withCredentials: true,
     headers: {
-        'API-KEY': '1cdd9f77-c60e-4af5-b194-659e4ebd5d41'
+        'API-KEY': 'bb249f66-6d8f-4cc5-9789-2a998b315ae1'
     }
 }
 const instance = axios.create({
